db46b2727edd762013363108fabfa49af8106afc
Uzair Akbar <uzairakbar025@gmail.com>
2025-04-18 12:13:40 -0400

    Load and save functions for metrics.

---
+++ streamlit/utils.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+
+# path to store metrics
+METRICS_FILE = 'streamlit/matching_metrics.json'
+
+
+def load_metrics() -> dict[str, list]:
+    '''
+    load metrics from file if present.
+    '''
+    if os.path.exists(METRICS_FILE):
+        with open(METRICS_FILE, 'r') as f:
+            return json.load(f)
+    return {
+        'latency': [],
+        'precision': [],
+        'recall': [],
+        'f1': [],
+        'bleu': [],
+        'rouge': []
+    }
+
+
+def save_metrics(metrics: dict[str, list]):
+    '''save metrics to file.'''
+    with open(METRICS_FILE, 'w') as f:
+        json.dump(metrics, f)
